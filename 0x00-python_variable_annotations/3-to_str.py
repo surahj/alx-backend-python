02690d72240685b8f45157ea9a9c7ab197197b8b
surahj <odebodeopeyemi@gmail.com>
2023-01-05 15:14:58 +0100

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env python3
+"""Contains a function that returns a string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns:
+        A string representation of the float
+    """
+    return str(n)
